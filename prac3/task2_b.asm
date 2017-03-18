9b670b160a450c7468985de3395d92ca32b0eceb
Aretha Peethamparam <aretha.peethamparam@gmail.com>
2017-03-18 21:03:21 +1100

    Finished task2_b.asm - tested and working

--- prac3/task2_b.asm
+++ prac3/task2_b.asm
@@ -27,65 +27,62 @@ add $a0, $t2, $t1
 addi $v0, $0, 9 #9 = allocate memory
 syscall
 
-sw $v0, the_list
-sw $t0, ($v0)
+sw $v0, the_list	# store allocated memory address in the_list
+sw $t0, ($v0)		# store size at first word in the_list
 
-#add items to the list
-#sw $0, i
+# loop to read and save into list
+
+# initialize counters
+li $t0, 0	# t0 = i = 0
+lw $t1, size
 
 loop: 
-lw $t0, count
-lw $t1, size
-lw $t7, i
+	#if i >= size endloop
+	bge $t0, $t1, eloop
 
-append:
-#if count >= size:
-bge $t0, $t1, loop2
-
-#append:
-# ask for item
-la $a0, stritem
-addi $v0, $0, 4
-syscall
-addi $v0, $0, 5
-syscall
-lw $t2, the_list #put the list address into $t2
-addi $t3, $0, 4 #$t3 = 4
-mult $t3, $t7 # 4 * i
-mflo $t4
-add $t4, $t4, $t3 #t4=i*4+4 
-add $t4, $t4, $t2 #$t4 points to next location in the list  
-sw $v0, ($t4) # store the next value 
-addi $t0, $t0, 1 #i=i+1 
-sw $t0, count 
-#print i
-la $a0, i
-addi $v0, $0, 4
-syscall
-j append 
+	# ask for item
+	la $a0, stritem
+	addi $v0, $0, 4
+	syscall
+	addi $v0, $0, 5
+	syscall
+	lw $t2, the_list #put the list address into $t2
+	addi $t3, $0, 4 #$t3 = 4
+	mult $t3, $t0 # 4 * i
+	mflo $t4
+	addi $t4, $t4, 4 #t4=i*4+4 
+	add $t4, $t4, $t2 #$t4 points to next location in the list  
+	sw $v0, ($t4) # store the next value 
+	#print i
+	la $a0, i
+	addi $v0, $0, 4
+	syscall
+	addi $t0, $t0, 1 #i=i+1 
+	j loop
+eloop:
 
 
 #print every second item in the list
-addi $t0, $0, 0
-loop2:
-lw $t1, the_list
-lw $t2, ($t1)
+addi $t0, $0, 0 #$t0 = index
+printloop:
+	lw $t1, the_list #$t1 = address of list
+	lw $t2, ($t1) #$t2 = size of list
 
-bge $t0, $t2, endloop2
+	bge $t0, $t2, endloop2 #if index >= size, exit
 
-addi $t3, $0, 4
-mult $t3, $t0
-mflo $t4
-add $t4,$t4,$t3 #$t4=$t0*4+4
-add $t4, $t4, $t1
-lw $a0, ($t4) 
-addi $v0, $0, 1  
-syscall # print current item 
-addi $a0, $0, 32 # print a space - ascii code 32  
-addi $v0, $0, 11 
-syscall 
-addi $t0, $t0, 1 #$t0=$t0+1 
-j loop2 
+	addi $t3, $0, 4 #$t3 = 4
+	mult $t3, $t0 
+	mflo $t4 #$t4 = 4*index
+	add $t4,$t4,$t3 #$t4 = 4 * index + 4
+	add $t4, $t4, $t1 #$t4 = the_list[i]
+	lw $a0, ($t4) 
+	addi $v0, $0, 1  
+	syscall # print current item 
+	addi $a0, $0, 32 # print a space - ascii code 32  
+	addi $v0, $0, 11 
+	syscall 
+	addi $t0, $t0, 2 #$t0=$t0+1 
+j printloop 
 
 endloop2:
 addi $v0, $0, 10
